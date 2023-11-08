1186c6a041e5c9ba6602648369ee75fba7831a8b
kanacb <kana@cloudbasha.com>
2023-11-08 11:49:06 +0800

    081123-1

--- gatewayscript/example-healthcheck.js
+++ gatewayscript/example-healthcheck.js
@@ -1,7 +1,7 @@
-var headers = require('header-metadata');
-var sf      = require('sprintf').sprintf;
+var headers = require("header-metadata");
+var sf = require("sprintf").sprintf;
 
-// set five parameters 
+// set five parameters
 //       grp        = lb group to control
 //       svr        = server name in lb group to toggle up/down
 //       svrport    = server port of server in lb group
@@ -10,40 +10,82 @@ var sf      = require('sprintf').sprintf;
 
 var grp = session.parameters.grp;
 var svr = session.parameters.svr;
-var svrport = (session.parameters.svrport) ? session.parameters.svrport:0;
-var healthport = (session.parameters.healthport) ? session.parameters.healthport:0;
+var svrport = session.parameters.svrport ? session.parameters.svrport : 0;
+var healthport = session.parameters.healthport
+  ? session.parameters.healthport
+  : 0;
 var evaluator = session.parameters.evaluator;
 
-console.debug("in custom healthcheck.js for server %s:%d from lb-group %s", svr, healthport, grp);
+console.debug(
+  "in custom healthcheck.js for server %s:%d from lb-group %s",
+  svr,
+  healthport,
+  grp
+);
 // In the default case, mark the server as 'unhealthy' if it responded with a status code that is not 200
 if (headers.current.statusCode != 200) {
-    session.reject(sf ("Server %s:%d from lb-group %s is unhealthy, status code: %d", svr, healthport, grp, headers.current.statusCode));
+  session.reject(
+    sf(
+      "Server %s:%d from lb-group %s is unhealthy, status code: %d",
+      svr,
+      healthport,
+      grp,
+      headers.current.statusCode
+    )
+  );
 } else {
-    // attempt to get the input data as JSON.  
-    session.input.readAsJSON(function (readAsJSONError, jsonResponse) {
-        // A JSON parsing error will result in a reject which === health check failure
-        if (readAsJSONError) {
-            session.reject(sf ("Server %s:%d from lb-group %s did not return at JSON response, readAsJSON error code: %d", 
-                           svr, healthport, grp, readAsJSONError.errorCode));
-        } else {
-            // since the response is JSON, parse the evaluator string into a JSON object and iterate through every property
-            // of the evaluator to make sure the value desired is in the actual response.  If any property in the evaluator
-            // fails to match the actual healthcheck response, a reject which === health check failure will be issued
-            try {
-                var evaluatorObject = JSON.parse(evaluator);
-                for (var property in evaluatorObject) {
-                    if (evaluatorObject[property] != jsonResponse[property]) {
-                        session.reject(sf ("Server %s:%d from lb-group %s healthcheck response invalid, expected: %s received %s", 
-                                           svr, healthport, grp, evaluatorObject[property], jsonResponse[property]));
-                    } else {
-                        console.debug("in custom healthcheck.js for server %s:%d from lb-group %s, successful evaluation of property %s", 
-                                      svr, healthport, grp, property);
-                    }
-                }
-            } catch (err) {
-                session.reject(sf ("Server %s:%d from lb-group %s evaluator is not a JSON object, errorMessage: %s", 
-                                   svr, healthport, grp, err.errorMessage));
-            }
+  // attempt to get the input data as JSON.
+  session.input.readAsJSON(function (readAsJSONError, jsonResponse) {
+    // A JSON parsing error will result in a reject which === health check failure
+    if (readAsJSONError) {
+      session.reject(
+        sf(
+          "Server %s:%d from lb-group %s did not return at JSON response, readAsJSON error code: %d",
+          svr,
+          healthport,
+          grp,
+          readAsJSONError.errorCode
+        )
+      );
+    } else {
+      // since the response is JSON, parse the evaluator string into a JSON object and iterate through every property
+      // of the evaluator to make sure the value desired is in the actual response.  If any property in the evaluator
+      // fails to match the actual healthcheck response, a reject which === health check failure will be issued
+      try {
+        var evaluatorObject = JSON.parse(evaluator);
+        for (var property in evaluatorObject) {
+          if (evaluatorObject[property] != jsonResponse[property]) {
+            session.reject(
+              sf(
+                "Server %s:%d from lb-group %s healthcheck response invalid, expected: %s received %s",
+                svr,
+                healthport,
+                grp,
+                evaluatorObject[property],
+                jsonResponse[property]
+              )
+            );
+          } else {
+            console.debug(
+              "in custom healthcheck.js for server %s:%d from lb-group %s, successful evaluation of property %s",
+              svr,
+              healthport,
+              grp,
+              property
+            );
+          }
         }
-    });
+      } catch (err) {
+        session.reject(
+          sf(
+            "Server %s:%d from lb-group %s evaluator is not a JSON object, errorMessage: %s",
+            svr,
+            healthport,
+            grp,
+            err.errorMessage
+          )
+        );
+      }
+    }
+  });
 }
